70f59932782a1f8c828c576db57ed56364678416
Michael Rosol <michaelrosol26@gmail.com>
2024-04-08 02:06:44 -0400

    Changes in Premium Page

--- src/pages/PremiumPage.tsx
+++ src/pages/PremiumPage.tsx
@@ -1,9 +1,21 @@
 
 import React from 'react'
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
+
 
 const PremiumPage = () => {
   return (
-    <div>PremiumPage</div>
+    <Authenticator>
+        {({ signOut}) => (
+            <div>
+                <h1>Hello, Welcome to Da Site </h1>
+                <h3>You are being authenticated...</h3>
+            </div>
+        )}
+
+    </Authenticator>
   )
 }
 
