9e5cae134de0da316088a9cbfc0df10bc3bfd1ea
Michael Rosol <michaelrosol26@gmail.com>
2024-04-12 01:30:16 -0400

    New Changes

--- src/pages/Textgen.tsx
+++ src/pages/Textgen.tsx
@@ -12,7 +12,7 @@ export function App({ signOut, user }: WithAuthenticatorProps) {
     <>
     <div className='container'>
       <h1 className='header'>Please Upload an Image</h1>
-
+      <h2>Trial</h2>
       <button className='upload-button'>Upload Image</button>
       <button onClick={signOut} className="absolute top-1 right-14  bg-indigo-700 hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded">Sign Out</button>
 
